d8842d266bb70183004ee856d951eb51299623ef
Almendra Gomez <kalmendra.24.93@gmail.com>
2025-02-12 11:48:40 +0100

    fixed main project

--- Proyecto/templates/main.php
+++ Proyecto/templates/main.php
@@ -3,7 +3,7 @@
         <img src="<?= $poster_url; ?>" width="200" alt="Poster de <?= $title; ?> style=" border-radius:16px" />
     </section>
     <hgroup>
-        <h3><?= $title; ?> - <?= $untilMessage; ?></h3>
+        <h3><?= $title ?> - <?= $until_message ?></h3>
         <p>Fecha de estreno: <?= $release_date; ?></p>
         <p>La siguiente es: <?= $following_production["title"] ?></p>
     </hgroup>
--- classes.php
+++ classes.php
@@ -1,18 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 class SuperHero
 {
     // Propiedades y los métodos
     // Promoted properties -> desde PHP 8
-    public function __construct(public $name, public $powers, public $planet) {}
+    public function __construct(private string $name, public array $powers, public string $planet) {}
 
     public function attack()
     {
         return "¡$this->name ataca con sus poderes!";
     }
 
+    public function show_all()
+    {
+        return get_object_vars($this);
+    }
     public function description()
     {
+        $powers = implode(", ", $this->powers);
         return "$this->name viene del planeta $this->planet y tiene los siguientes poderes: $this-powers";
     }
 }
+
+$hero = new SuperHero("Superman", ["Superfuerza", "visión térmica", "volar"], "Krypton");
+
+echo $hero->description();
